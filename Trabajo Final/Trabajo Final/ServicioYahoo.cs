f4526327043a7ebd3a5c616982d9ec30de36bef3
BrianFellin <brianfellin@hotmail.com>
2014-12-01 20:15:46 -0300

    servicio yahoo

---
+++ Trabajo Final/Trabajo Final/ServicioYahoo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace Trabajo_Final
+{
+    class ServicioYahoo : IServicio
+    {
+        private string iNombre;
+
+        public void ServicioYahoo(string pNombre)
+        {
+            this.iNombre = pNombre;
+        }
+
+        public void EnviarMail(MailMessage pMail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<System.Net.Mail.MailMessage> RecibirMail()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
