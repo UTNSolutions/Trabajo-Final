5ca771a147282a071a9e5a46bcf9442eebac26a5
matiadr <mati.d@live.com.ar>
2014-12-01 19:52:10 -0300

    interfaz IServicio

---
+++ Trabajo Final/Trabajo Final/IServicio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace Trabajo_Final
+{
+    /// <summary>
+    /// Representa operaciones para el manejo de una cuenta de correo
+    /// </summary>
+    interface IServicio
+    {
+        //Representa el nombre del servicio de correo, ej: Yahoo,Gmail
+        String iNombre;
+
+        /// <summary>
+        /// Permite enviar un mail
+        /// </summary>
+        void EnviarMail();
+
+        /// <summary>
+        /// Permite recibir emails
+        /// </summary>
+        IList<MailMessage> RecibirMail();
+    }
+}
