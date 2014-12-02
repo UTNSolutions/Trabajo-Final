4fa1689735902717ca3bc987628a847820958bc8
matiadr <mati.d@live.com.ar>
2014-12-02 15:04:30 -0300

    cambiosss

--- Trabajo Final/Trabajo Final/Persistencia/SQLServerEmailDAO.cs
+++ Trabajo Final/Trabajo Final/Persistencia/SQLServerEmailDAO.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Trabajo_Final.Persistencia
 {
     class SQLServerEmailDAO : IMailDAO
     {
+        private SqlConnection iConexion;
+        private SqlTransaction iTransaccion;
+
+        public SQLServerEmailDAO(SqlConnection pConexion, SqlTransaction pTransaccion)
+        {
+            this.iConexion = pConexion;
+            this.iTransaccion = pTransaccion;
+
+        }
+        public void Insertar(EmailDTO pEmail)
+        {
+            
+
+        }
+
+        public void Eliminar(EmailDTO pEmail)
+        {
+       
+        }
+
+        public IList<EmailDTO> Obtener(CuentaDTO pCuenta)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
