4e15d29be8ed8f674ccf5434f694ed046e678ac8
matiadr <mati.d@live.com.ar>
2014-12-04 01:34:06 -0300

    cambioss

---
+++ Trabajo Final/Trabajo Final/UI/FormAdministrarCuentas.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Trabajo_Final.Controladores;
+using Trabajo_Final.Excepciones;
+
+namespace Trabajo_Final.UI
+{
+    public partial class FormAdministrarCuentas : Form
+    {
+        public FormAdministrarCuentas()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAdministrarCuentas_Load(object sender, EventArgs e)
+        {
+            cbServicio.SelectedIndex = 0;
+            //Cargo el data grid de cuentas con las cuentas obtenidas de la base de datos
+            dgCuentas.DataSource = Fachada.Instancia.ObtenerCuentas();
+        }
+
+        /// <summary>
+        /// Permite dar de alta una cuanta de correo electronico
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AltaCuenta(object sender, EventArgs e)
+        {
+            if (tbCuenta.Text == "" | tbMail.Text == "")
+            {
+                MessageBox.Show("Falta completar datos obligatorios", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {   
+                    Fachada.Instancia.AltaCuenta(tbCuenta.Text, tbMail.Text, cbServicio.SelectedItem.ToString(), tbContraseña.Text);
+                }
+                catch (DAOExcepcion ex)
+                {
+                    MessageBox.Show(ex.Message, "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (NombreCuentaExcepcion ex)
+                {
+                    MessageBox.Show(ex.Message, "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
---
+++ Trabajo Final/Trabajo Final/UI/FormPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo_Final.UI
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormAcercaDe formAcercaDe = new FormAcercaDe();
+            formAcercaDe.Show();
+        }
+
+        private void cuentasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormAdministrarCuentas formAdminCuentas = new FormAdministrarCuentas();
+            formAdminCuentas.Show();
+        }
+    }
+}
